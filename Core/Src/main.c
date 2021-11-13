299573f05dfc1c05824a36a46726381a730eef58
Lajob <ustym.hanyk@ukr.net>
2021-11-13 09:30:35 +0200

    Increased timers to 72mHz, hence changed pwm duty cycle values

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -101,10 +101,10 @@ void WS2812_Send (void)
 		{
 			if (color&(1<<i))
 			{
-				pwmData[indx] = 6; // if the bit is 1, the duty cycle is 64% (64% of 90)
+				pwmData[indx] = 57; // if the bit is 1, the duty cycle is 64%
 			}
 
-			else pwmData[indx] = 3;  // if the bit is 0, the duty cycle is 32%
+			else pwmData[indx] = 28;  // if the bit is 0, the duty cycle is 32%
 
 			indx++;
 		}
@@ -236,7 +236,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -246,11 +246,11 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
